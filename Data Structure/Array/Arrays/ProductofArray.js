c16be2d0b5eba02c13ad0b999b91ead7c2a37b53
Pankaj-026 <pankajgupta1063@gmail.com>
2025-04-09 21:09:07 +0530

    Add productOfArray function to calculate products of array elements excluding self

---
+++ Data Structure/Array/Arrays/ProductofArray.js
@@ -0,0 +1,19 @@
+let a = [1, 2, 3, 4];
+
+function productOfArray(arr) {
+  let result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let product = 1;
+    for (let j = 0; j < arr.length; j++) {
+      if (i !== j) {
+        product *= arr[j];
+      }
+    }
+    result.push(product);
+  }
+
+  return result;
+}
+
+console.log(productOfArray(a));

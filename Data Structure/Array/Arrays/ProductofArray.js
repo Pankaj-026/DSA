726599f04bcf5a696bf6d92ab0dace1e3f442450
Pankaj-026 <pankajgupta1063@gmail.com>
2025-04-10 00:10:28 +0530

    Add optimized approaches for product of array function

--- Data Structure/Array/Arrays/ProductofArray.js
+++ Data Structure/Array/Arrays/ProductofArray.js
@@ -18,3 +18,49 @@ function productOfArray(arr) {
 }
 
 console.log(productOfArray(a));
+
+// Optimized Approach
+let b = [1, 2, 3, 4];
+
+function productOfArrayOptimized(arr) {
+  let result = new Array(arr.length);
+  let prefix = [];
+  let suffix = [];
+  prefix[0] = 1;
+  suffix[arr.length - 1] = 1;
+
+  for (let i = 1; i < arr.length; i++) {
+    prefix[i] = arr[i - 1] * prefix[i - 1];
+  }
+
+  for (let i = arr.length - 2; i >= 0; i--) {
+    suffix[i] = arr[i + 1] * suffix[i + 1];
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] = prefix[i] * suffix[i];
+  }
+
+  return result;
+}
+
+console.log(productOfArrayOptimized(a));
+
+// cleaner version of the optimized approach
+function productOfArrayCleaner(arr) {
+  let result = new Array(arr.length).fill(1);
+  let prefix = 1;
+  let suffix = 1;
+
+  for (let i = 0; i < arr.length; i++) {
+    result[i] *= prefix;
+    prefix *= arr[i];
+  }
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result[i] *= suffix;
+    suffix *= arr[i];
+  }
+
+  return result;
+}

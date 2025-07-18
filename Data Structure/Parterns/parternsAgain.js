c4cffbe3372b6908304b233bac556cac60eb9da0
Pankaj-026 <pankajgupta1063@gmail.com>
2025-07-19 00:04:36 +0530

    Right-Angled Star Triangle Partern

--- Data Structure/Parterns/parternsAgain.js
+++ Data Structure/Parterns/parternsAgain.js
@@ -1,7 +1,20 @@
-for (let i = 0; i < 4; i++) {
-    let row = "";
-    for (let j = 0; j < 4; j++) {
-        row += "*"
+// Todo: Square Partern
+
+// for (let i = 0; i < 4; i++) {
+//     let row = "";
+//     for (let j = 0; j < 4; j++) {
+//         row += "*"
+//     }
+//     console.log(row);
+// }
+
+// Todo: Right-Angled Star Triangle
+
+for (let i = 0; i <= 4; i++) {
+    let row = ""
+    for (let j = 0; j < i; j++) {
+        row += "*";
     }
     console.log(row);
+    
 }

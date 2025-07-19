3b7cb8e348c35697ec3b65e16f746ea39c429df2
Pankaj-026 <pankajgupta1063@gmail.com>
2025-07-19 23:11:00 +0530

    Right Angle Triangle of Repeated Number

--- Data Structure/Parterns/parternsAgain.js
+++ Data Structure/Parterns/parternsAgain.js
@@ -21,11 +21,21 @@
 
 // Todo: Print a Right-Angled Number Triangle
 
-for (let i = 0; i <= 4; i++) {
-    let row = ""
-    for (let j = 1; j <= i; j++) {
-        row += j
-    }
-    console.log(row);
+// for (let i = 0; i <= 4; i++) {
+//     let row = ""
+//     for (let j = 1; j <= i; j++) {
+//         row += j
+//     }
+//     console.log(row);
 
+// }
+
+// Todo: Right Angle Triangle of Repeated Number
+
+for (let i = 1; i <= 4; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += i;
+  }
+  console.log(row);
 }

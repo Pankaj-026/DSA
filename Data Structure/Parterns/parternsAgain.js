34982b81e0787cf5221bf14738998d2226f67df3
Pankaj-026 <pankajgupta1063@gmail.com>
2025-07-21 16:57:16 +0530

    Right-Aligned Right-Angled Triangle of Stars

--- Data Structure/Parterns/parternsAgain.js
+++ Data Structure/Parterns/parternsAgain.js
@@ -43,10 +43,24 @@
 
 // Todo:  Right Angle Triangle of Number in Reverse
 
-for (let i = 0; i < 4; i++) {
+// for (let i = 0; i < 4; i++) {
+//   let row = "";
+//   for (let j = 1; j <= 4 - i; j++) {
+//     row += j;
+//   }
+//   console.log(row);
+// }
+
+// Todo: Right-Aligned Right-Angled Triangle of Stars
+let n = 4
+
+for (let i = 0; i < n; i++) {
   let row = "";
-  for (let j = 1; j <= 4 - i; j++) {
-    row += j;
+  for (let j = 1; j < n - i; j++) {
+    row += " "
+  }
+  for (let k = n; k >= n-i; k--) {
+    row += "*"
   }
   console.log(row);
 }
